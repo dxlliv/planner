dfb5c581fb74a8218b669b66323fca6f094c99a0
dxlliv <hello@dxlliv.dev>
2023-10-19 05:46:14 +0200

    Update dist folder

--- assets/ProfileAvatar.vue_vue_type_script_setup_true_lang-27b9a6cd.js
+++
@@ -1,1 +0,0 @@
-import{e as r,r as a,o as n,h as s,w as c,f as _}from"./index-9e0a579a.js";const i=r({__name:"ProfileAvatar",props:{avatar:Object},setup(t){return(p,v)=>{const e=a("v-img"),o=a("v-avatar");return n(),s(o,{class:"igp-profile-avatar"},{default:c(()=>[_(e,{src:t.avatar.path},null,8,["src"])]),_:1})}}});export{i as _};
