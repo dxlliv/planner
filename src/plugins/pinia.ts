c2231ef69372848cb56198d3d2c82a144b09bde8
dxlliv <hello@dxlliv.dev>
2023-11-20 01:15:04 +0100

    Add pinia-plugin-persistedstate library

--- src/plugins/pinia.ts
+++ src/plugins/pinia.ts
@@ -1,5 +1,10 @@
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 export default {
   install: (app: App) => {
-    app.use(createPinia());
+    const pinia = createPinia()
+    pinia.use(piniaPluginPersistedstate)
+
+    app.use(pinia);
   },
 };
