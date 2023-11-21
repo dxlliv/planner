c5e03cb24b35e4d352d518e8c18c16835fce8f0e
dxlliv <hello@dxlliv.dev>
2023-11-21 16:07:38 +0100

    Add tooltip to ProfileSelectorAdd.vue

--- src/components/Profile/Profile/ProfileSelector/ProfileSelectorAdd.vue
+++ src/components/Profile/Profile/ProfileSelector/ProfileSelectorAdd.vue
@@ -1,13 +1,23 @@
 <template>
   <ProfileSelector add @click="onProfileAdd">
-    <ProfileAvatar>
+    <ProfileAvatar >
       <v-icon size="48px" icon="mdi-plus" color="grey-lighten-1" />
+      <v-tooltip activator="parent" location="bottom" :offset="[14, 0]">
+        Import profile
+      </v-tooltip>
     </ProfileAvatar>
   </ProfileSelector>
 </template>
 
 <script setup lang="ts">
+//const configUserStore = useConfigUserStore()
+
 async function onProfileAdd() {
+
+
+
+
+  //configUserStore.setUserConfig(config)
 }
 </script>
 
