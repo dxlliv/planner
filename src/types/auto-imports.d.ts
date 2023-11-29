ae9daabe9dc34964d4bf32fcac1d24cb6fd2d77e
dxlliv <hello@dxlliv.dev>
2023-11-29 05:17:04 +0100

    Update dist folder

--- src/types/auto-imports.d.ts
+++ src/types/auto-imports.d.ts
@@ -142,4 +142,5 @@ declare global {
 declare global {
   // @ts-ignore
   export type { Component, ComponentPublicInstance, ComputedRef, ExtractDefaultPropTypes, ExtractPropTypes, ExtractPublicPropTypes, InjectionKey, PropType, Ref, VNode, WritableComputedRef, App } from 'vue'
+  import('vue')
 }
