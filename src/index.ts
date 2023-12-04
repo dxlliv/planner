81775efde9ba514b7d9557ec95b58bf26a01065b
dxlliv <hello@dxlliv.dev>
2023-12-04 03:51:33 +0100

    Export createPlanner fn for future developments

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {plugins} from "./plugins";
+import App from "./App.vue";
+import {createApp} from "vue";
+
+class Planner {
+    readonly app
+
+    constructor() {
+        this.app = createApp(App)
+            .use(plugins)
+            .mount("#app")
+    }
+}
+
+export function createPlanner() {
+    return new Planner()
+}
