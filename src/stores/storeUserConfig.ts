eeeeb017a5da062a085dc924d5cfa9a71efef68a
dxlliv <hello@dxlliv.dev>
2023-12-21 17:16:18 +0100

    Support for remote user configurations

--- src/stores/storeUserConfig.ts
+++ src/stores/storeUserConfig.ts
@@ -11,8 +11,17 @@ export const useUserConfigStore = defineStore("user/config", () => {
     // for each user defined in the root config.json
     for await (const userPath of plannerConfig.users) {
 
+      let userConfigFullPath = ''
+
+      // is local or remote config?
+      if (userPath.startsWith('http')) {
+        userConfigFullPath = userPath
+      } else{
+        userConfigFullPath = `user/${userPath}/config.json`
+      }
+
       // fetch user config from its local/remote path
-      const rawUser = await fetchRemoteUserConfig(`user/${userPath}/config.json`)
+      const rawUser = await fetchRemoteUserConfig(userConfigFullPath)
       const platform: string = getPlatformFromUserPath(userPath)
 
       // load users
