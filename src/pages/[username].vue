bd27acc0f32d0fdde62ea12b727157fa41755ad2
dxlliv <hello@dxlliv.dev>
2024-03-02 21:54:12 +0100

    Minor improvements

--- src/pages/[username].vue
+++ src/pages/[username].vue
@@ -4,7 +4,6 @@ const route = useRoute()
 const userStore = useUserStore()
 
 const userId: string = route.params.username.toString()
-
 const user = userStore.getUser(userId, plannerConfig.platform.default)
 
 if (user) {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <PagePlannerIndex />
 </template>
-
-<style scoped lang="scss">
-
-</style>
