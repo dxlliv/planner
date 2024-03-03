b2d31ec5fc1cf01c13f9b5c4670e269ad875dcd0
dxlliv <hello@dxlliv.dev>
2024-03-03 23:37:49 +0100

    Update dist folder

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "dev"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+import cfg0 from "/home/dxlliv/Development/Personal/planner/layer/app.config.ts"
+
+export default /*@__PURE__*/ defuFn(cfg0, inlineConfig)
---
+++ .nuxt/i18n.options.mjs
@@ -0,0 +1,139 @@
+
+// @ts-nocheck
+
+
+export const localeCodes =  []
+
+export const localeLoaders = {
+
+}
+
+export const vueI18nConfigs = [
+  () => import("../i18n.config.ts?hash=bffaebcb&config=1" /* webpackChunkName: "i18n_config_bffaebcb" */)
+]
+
+export const nuxtI18nOptions = {
+  "experimental": {
+    "localeDetector": ""
+  },
+  "bundle": {
+    "compositionOnly": true,
+    "runtimeOnly": false,
+    "fullInstall": true,
+    "dropMessageCompiler": false
+  },
+  "compilation": {
+    "jit": true,
+    "strictMessage": true,
+    "escapeHtml": false
+  },
+  "customBlocks": {
+    "defaultSFCLang": "json",
+    "globalSFCScope": false
+  },
+  "vueI18n": "",
+  "locales": [],
+  "defaultLocale": "",
+  "defaultDirection": "ltr",
+  "routesNameSeparator": "___",
+  "trailingSlash": false,
+  "defaultLocaleRouteNameSuffix": "default",
+  "strategy": "prefix_except_default",
+  "lazy": false,
+  "langDir": null,
+  "rootRedirect": null,
+  "detectBrowserLanguage": {
+    "alwaysRedirect": false,
+    "cookieCrossOrigin": false,
+    "cookieDomain": null,
+    "cookieKey": "i18n_redirected",
+    "cookieSecure": false,
+    "fallbackLocale": "",
+    "redirectOn": "root",
+    "useCookie": true
+  },
+  "differentDomains": false,
+  "baseUrl": "",
+  "dynamicRouteParams": false,
+  "customRoutes": "page",
+  "pages": {},
+  "skipSettingLocaleOnNavigate": false,
+  "types": "composition",
+  "debug": false,
+  "parallelPlugin": false,
+  "i18nModules": []
+}
+
+export const nuxtI18nOptionsDefault = {
+  "experimental": {
+    "localeDetector": ""
+  },
+  "bundle": {
+    "compositionOnly": true,
+    "runtimeOnly": false,
+    "fullInstall": true,
+    "dropMessageCompiler": false
+  },
+  "compilation": {
+    "jit": true,
+    "strictMessage": true,
+    "escapeHtml": false
+  },
+  "customBlocks": {
+    "defaultSFCLang": "json",
+    "globalSFCScope": false
+  },
+  "vueI18n": "",
+  "locales": [],
+  "defaultLocale": "",
+  "defaultDirection": "ltr",
+  "routesNameSeparator": "___",
+  "trailingSlash": false,
+  "defaultLocaleRouteNameSuffix": "default",
+  "strategy": "prefix_except_default",
+  "lazy": false,
+  "langDir": null,
+  "rootRedirect": null,
+  "detectBrowserLanguage": {
+    "alwaysRedirect": false,
+    "cookieCrossOrigin": false,
+    "cookieDomain": null,
+    "cookieKey": "i18n_redirected",
+    "cookieSecure": false,
+    "fallbackLocale": "",
+    "redirectOn": "root",
+    "useCookie": true
+  },
+  "differentDomains": false,
+  "baseUrl": "",
+  "dynamicRouteParams": false,
+  "customRoutes": "page",
+  "pages": {},
+  "skipSettingLocaleOnNavigate": false,
+  "types": "composition",
+  "debug": false,
+  "parallelPlugin": false
+}
+
+export const normalizedLocales = []
+
+export const NUXT_I18N_MODULE_ID = "@nuxtjs/i18n"
+export const parallelPlugin = false
+export const isSSG = false
+
+export const STRATEGIES = {
+  "PREFIX": "prefix",
+  "PREFIX_EXCEPT_DEFAULT": "prefix_except_default",
+  "PREFIX_AND_DEFAULT": "prefix_and_default",
+  "NO_PREFIX": "no_prefix"
+}
+export const DEFAULT_LOCALE = ""
+export const DEFAULT_STRATEGY = "prefix_except_default"
+export const DEFAULT_TRAILING_SLASH = false
+export const DEFAULT_ROUTES_NAME_SEPARATOR = "___"
+export const DEFAULT_LOCALE_ROUTE_NAME_SUFFIX = "default"
+export const DEFAULT_DETECTION_DIRECTION = "ltr"
+export const DEFAULT_BASE_URL = ""
+export const DEFAULT_DYNAMIC_PARAMS_KEY = "nuxtI18n"
+
+
