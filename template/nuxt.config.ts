ffd14d25b97120f009ea00e5b336e60eb43260e1
dxlliv <hello@dxlliv.dev>
2024-03-05 00:34:38 +0100

    Add template files

---
+++ template/nuxt.config.ts
@@ -0,0 +1,25 @@
+import { createResolver } from "@nuxt/kit";
+
+const { resolve } = createResolver(import.meta.url);
+
+export default defineNuxtConfig({
+  ssr: false,
+  alias: {
+    "~": resolve(__dirname, "."),
+    "@": resolve(__dirname, "./src")
+  },
+  srcDir: "./src",
+  extends: ["plxnner"],
+  app: {
+    head: {
+      title: "Planner",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" }
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    }
+  },
+  build: {
+    transpile: ["vuetify"]
+  }
+});
---
+++ template/planner.config.ts
@@ -0,0 +1,14 @@
+export default {
+  users: [
+    "instagram/dxlliv"
+  ],
+  platform: {
+    default: "instagram"
+  },
+  locale: "en",
+  features: {
+    profileEditor: true,
+    mediaSort: true,
+    mediaEditor: true
+  }
+}
