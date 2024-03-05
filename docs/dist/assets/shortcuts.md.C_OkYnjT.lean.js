4c51eca9432371f7b60c497dd3763f7a9d46cb42
dxlliv <hello@dxlliv.dev>
2024-03-05 01:47:18 +0100

    Update dist folder

---
+++ docs/dist/assets/shortcuts.md.C_OkYnjT.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as r,a2 as a}from"./chunks/framework.Bz3FD1ko.js";const p=JSON.parse('{"title":"Shortcuts","description":"","frontmatter":{},"headers":[],"relativePath":"shortcuts.md","filePath":"shortcuts.md"}'),s={name:"shortcuts.md"},o=a("",6),n=[o];function h(c,i,l,u,d,_){return r(),e("div",null,n)}const f=t(s,[["render",h]]);export{p as __pageData,f as default};
