7f296ad1a38515769c018a9d702b50c2feea7bd0
dxlliv <hello@dxlliv.dev>
2024-03-24 06:24:33 +0100

    Improve text in MainDeluxe.vue

--- core/components/Main/MainDeluxe.vue
+++ core/components/Main/MainDeluxe.vue
@@ -9,15 +9,15 @@ import { project } from "../../project/project";
     <v-row no-gutters class="ma-0 fill-height">
       <v-col align-self="center" class="text-center">
         <p>
-          Enjoy the free to use open-source version!<br />
+          Enjoy the free to use open-source edition!<br />
           <router-link class="font-weight-black" to="/">Play with this Instance</router-link>
-          or <a class="font-weight-black" :href="project.docs">Host Yours</a>.
+          or <a class="font-weight-black" :href="project.source">Host Yours</a>.
         </p>
         <v-divider class="my-8" />
         <p>
           Do you like this project and want to have<br />
-          a backup of your Instagram profile enriched<br />
-          with a special edition of this planner?
+          a tailor-made clone of your Instagram profile<br />
+          that highlights the best you've created?
         </p>
         <p class="mt-5">
           Request a Deluxe Planner crafted by <a class="font-weight-bold" :href="project.deluxe">dxlliv</a>.<br />
