92abaecfcaf3c5e9027862abe2aa747138b479fb
dxlliv <hello@dxlliv.dev>
2024-03-30 21:11:47 +0100

    Remove ssr:true from core

--- core/nuxt.config.ts
+++ core/nuxt.config.ts
@@ -7,7 +7,6 @@ const { resolve } = createResolver(import.meta.url);
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true,
   alias: {
     "£": resolve(__dirname, ".")
   },
