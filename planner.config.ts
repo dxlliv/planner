b22225e343974022db3331243e94e5e19161d382
dxlliv <hello@dxlliv.dev>
2024-04-05 00:45:39 +0200

    Update dxvfpv external profile path

--- planner.config.ts
+++ planner.config.ts
@@ -1,7 +1,7 @@
 export default {
   users: [
     "instagram/dxlliv",
-    "https://raw.githubusercontent.com/dxlliv/instagram/main/src/public/user/instagram/dxvfpv/config.json"
+    "https://raw.githubusercontent.com/dxlliv/instagram/main/public/user/instagram/dxvfpv/config.json"
   ],
   platform: {
     default: "instagram"
