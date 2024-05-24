0728dfb6668e2bd99c924e57659eda1d74e0f3cc
dxlliv <hello@dxlliv.dev>
2024-05-24 04:38:15 +0200

    Lock user scalability in nuxt.config.ts

--- core/nuxt.config.ts
+++ core/nuxt.config.ts
@@ -12,7 +12,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1" }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
       ],
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
     }
