808194f8add82b607cf649d33d08dfaf385490fb
dxlliv <hello@dxlliv.dev>
2024-11-30 12:21:42 +0100

    Implement PostDialog.vue

--- core/components/Instagram/Post/InstagramPost.vue
+++ core/components/Instagram/Post/InstagramPost.vue
@@ -68,32 +68,18 @@ watch(() => props.media.isDetailView, value => {
 
     <client-only>
 
-      <v-dialog v-model="postDetailDialog">
+      <PostDialog v-model="postDetailDialog">
         <InstagramPostDetail
           :media="media"
           :user="user"
         />
-      </v-dialog>
+      </PostDialog>
 
     </client-only>
   </div>
 </template>
 
 <style scoped lang="scss">
-@import "vuetify/lib/styles/settings/_variables";
-
-.v-dialog {
-  width: calc(100% - 64px - 24px);
-  max-width: 1200px;
-  max-height: 913px;
-
-  @media #{map-get($display-breakpoints, 'xs')} {
-    width: 100%;
-    max-width: 100%;
-    margin: 0;
-  }
-}
-
 .ig-post-container {
   width: 100%;
 }
